8af8491376bf4aa3fd4419b0b66011a6821254ac
Navemics <navemics@gmail.com>
2020-09-17 14:53:32 +0530

    Added Blur View

--- Sources/LottieUI/LottieUI.swift
+++ Sources/LottieUI/LottieUI.swift
@@ -1,3 +1,13 @@
 struct LottieUI {
     var text = "Hello, World!"
 }
+
+struct Blurview : UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<Blurview>) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterialLight))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Blurview>) {    
+    }
+}
