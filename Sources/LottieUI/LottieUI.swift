6c77fbf64801a72d6c8a8383e02d9fe2e4927a68
Navemics <navemics@gmail.com>
2020-09-17 15:00:52 +0530

    Bug Fixes

--- Sources/LottieUI/LottieUI.swift
+++ Sources/LottieUI/LottieUI.swift
@@ -1,8 +1,12 @@
+import SwiftUI
+
 struct LottieUI {
     var text = "Hello, World!"
 }
 
+@available(iOS 13.0, *)
 struct Blurview : UIViewRepresentable {
+    
     func makeUIView(context: UIViewRepresentableContext<Blurview>) -> UIVisualEffectView {
         let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterialLight))
         return view
